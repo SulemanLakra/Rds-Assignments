bd9f8d111e9ff4c24e92f337011dd640a073caef
Suleman Lakra <169129299+SulemanLakra@users.noreply.github.com>
2025-01-31 22:49:46 +0530

    Create sol21.R

---
+++ Unit01_assignments/sol21.R
@@ -0,0 +1,7 @@
+seq20 <- 15:25
+rep4_2 <- rep(4.2, 2)
+single_minus5 <- -5
+seq_12 <- seq(200, length(seq1), length.out = 12)
+vec20 <- c(seq20, rep4_2, single_minus5, seq_12)
+
+length(vec20) == 25
